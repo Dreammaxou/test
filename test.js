e6e920aa12f378b53ec61860d131660279029325
Dreammaxou <maxime-du-08@laposte.net>
2014-11-11 20:59:54 +0100

    Update and rename README.md to test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+function AfficheBT(){
+	document.getElementsByClassName("cb_actions")[0].innerHTML .= "<div class=\"cb_btn action\" show=\"smileys\"><span class=\"icon icon-smiley2\">::before</span></div>"
+	
+]}
+
+AfficheBT();
