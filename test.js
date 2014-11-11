2a3c01fc54cccdce92d2efbdc871d2f6e5267bf8
Dreammaxou <maxime-du-08@laposte.net>
2014-11-11 21:12:15 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 function AfficheBT(){
-	document.getElementsByClassName("cb_actions")[0].innerHTML .= "<div class=\"cb_btn action\" show=\"smileys\"><span class=\"icon icon-smiley2\">::before</span></div>"
+	document.getElementsByClassName("cb_actions")[0].innerHTML .= "<div class='cb_btn action' show='smileys'><span class='icon icon-smiley2'>::before</span></div>"
 	
 }
 
